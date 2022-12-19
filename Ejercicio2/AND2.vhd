875cd99f5a8d9d5979c5b9805fa7abab1e0004bf
Jonas Lara <adrianlr2515@outlook.com>
2022-12-19 12:59:49 -0600

    UPDATE

--- Ejercicio2/AND2.vhd
+++ Ejercicio2/AND2.vhd
@@ -1,18 +1,3 @@
--- library ieee;
--- use ieee.std_logic_1164.all;
-
--- entity AND2 is
---     port(a_i : in  std_logic;
---          b_i : in  std_logic;
---          y_o : out std_logic);
--- end entity AND2;
-     
--- architecture FlujoDeDatos of AND2 is begin
-     
---     y_o <= a_i and b_i;
-     
--- end architecture FlujoDeDatos;
-
 entity AND2 is
 end entity;
      
